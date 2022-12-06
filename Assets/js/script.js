f299656411aaeaefe933bc48152a84998f018b4a
David Mackey <davidmackey92@gmail.com>
2022-12-05 19:05:14 -0600

    clean up on aisle 5 aka: nixed unused images, relinked bg-img and nasa-logo

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,21 +1,21 @@
 //Earth date, upper right
 var today = document.getElementById('dateToday');
-today.innerHTML=moment().format('LLLL');
+today.innerHTML = moment().format('LLLL');
 
 //DONKI
 //https://api.nasa.gov/DONKI/notifications?startDate=2014-05-01&endDate=2014-05-08&type=all&api_key=DEMO_KEY
 
 var todaysDate = 'startDate' + moment().format('L');
-var apiKey='&api_key=jNaTZmpOkTNGqbyCyZipimLVQEiNfeKjWvkcEMe3';
+var apiKey = '&api_key=jNaTZmpOkTNGqbyCyZipimLVQEiNfeKjWvkcEMe3';
 var dataUrl = "https://api.nasa.gov/DONKI/notifications?" + todaysDate + apiKey;
-var msgBody=document.getElementById('donki');
+var msgBody = document.getElementById('donki');
 
 fetch(dataUrl)
-    .then(function (response){
-        return response.json();
-    })
-    .then (function(data){
-        console.log(data[0].messageBody);
-        var jargon=data[0].messageBody.replace(/\##/g,"</br>");
-        msgBody.innerHTML=jargon;
-    })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data[0].messageBody);
+    var jargon = data[0].messageBody.replace(/\##/g, "</br>");
+    msgBody.innerHTML = jargon;
+  })
