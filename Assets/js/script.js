9ce9585303457e6c019357cc3cf0f24d6fab417b
David Mackey <davidmackey92@gmail.com>
2022-12-06 18:59:40 -0600

    tweaked bs layout

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,7 +4,7 @@ var today = document.getElementById('dateToday');
 // today.innerHTML=moment().format('LLLL');
 
 
-var apiKeyPicture =  "vbBdCkMZKqwb4qJwGH4og1wD1L5EJKlW5gF4m9m4"
+var apiKeyPicture = "vbBdCkMZKqwb4qJwGH4og1wD1L5EJKlW5gF4m9m4"
 
 
 var photoURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2022-8-3&api_key=" + apiKeyPicture
@@ -28,35 +28,35 @@ var roverStatus = document.getElementById("status")
 let modalBtn = document.getElementById("modal-btn")
 let modal = document.querySelector(".modal")
 let closeBtn = document.querySelector(".close-btn")
-modalBtn.onclick = function(){
+modalBtn.onclick = function () {
   modal.style.display = "block"
 }
-closeBtn.onclick = function(){
+closeBtn.onclick = function () {
   modal.style.display = "none"
 }
-window.onclick = function(e){
-  if(e.target == modal){
+window.onclick = function (e) {
+  if (e.target == modal) {
     modal.style.display = "none"
   }
 }
 
 fetch(photoURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then (function(data) {
-        console.log(data.photos[0].earth_date)
-        console.log(data.photos[0].sol)
-        console.log(data.photos[0].img_src)
-        marsImgSrc.src=data.photos[0].img_src
-        
-        solInfo.innerText = "Rover: " + data.photos[0].rover.name
-        earthDate.innerText = "Earth Date: " + data.photos[0].earth_date
-        camera.innerText = "Camera: " + data.photos[0].camera.full_name
-        roverStatus.innerText = "Rover Status: " + data.photos[0].rover.status
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data.photos[0].earth_date)
+    console.log(data.photos[0].sol)
+    console.log(data.photos[0].img_src)
+    marsImgSrc.src = data.photos[0].img_src
+
+    solInfo.innerText = "Rover: " + data.photos[0].rover.name
+    earthDate.innerText = "Earth Date: " + data.photos[0].earth_date
+    camera.innerText = "Camera: " + data.photos[0].camera.full_name
+    roverStatus.innerText = "Rover Status: " + data.photos[0].rover.status
 
 
-    })
+  })
 
 //Earth date, upper right
 var today = document.getElementById('dateToday');
