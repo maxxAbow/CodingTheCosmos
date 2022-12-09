e5927ebab00f3e0114b17aa4532e6967fa7dbb14
Lauren Bentley <laurendebentley@gmail.com>
2022-12-09 10:05:21 -0500

    added jquery UI for calender and re-added modal button

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -23,18 +23,31 @@ var camera = document.getElementById('camera')
 var roverStatus = document.getElementById("status")
 
 
+let modalBtn = document.getElementById("modal-btn")
 let modal = document.querySelector(".modal")
 let closeBtn = document.querySelector(".close-btn")
-closeBtn.onclick = function () {
+
+modalBtn.onclick = function(){
+  modal.style.display = "block"
+  
+}
+closeBtn.onclick = function(){
   modal.style.display = "none"
 }
-window.onclick = function (e) {
-  if (e.target == modal) {
+window.onclick = function(e){
+  if(e.target == modal){
     modal.style.display = "none"
   }
+
+
 }
 
-fetch(photoURL)
+
+$(function() {
+    $("#date-picker").datepicker()
+  })
+
+  fetch(photoURL)
   .then(function (response) {
     return response.json();
   })
