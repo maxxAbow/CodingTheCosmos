8939f56cf6e79862f0512eed26d0c30772b902e5
Lauren Bentley <laurendebentley@gmail.com>
2022-12-11 12:04:38 -0500

    added select date feature

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -42,6 +42,89 @@ window.onclick = function(e){
 
 }
 
+// converts months to numbered format for select-date api call
+
+
+  const monthObj = {
+  Jan: "1",
+  Feb: "2",
+  Mar: "3",
+  Apr: "4",
+  May: "5",
+  Jun: "6",
+  Jul: "7",
+  Aug: "8",
+  Sep: "9",
+  Oct: "10",
+  Nov: "11",
+  Dec: "12" 
+} 
+
+  function me(){
+    var text = $( "#date-picker" ).datepicker("getDate").toString()
+    var newText = text.slice(4,7)
+    var yearNumber = text.slice(11,15)
+    var dayNumber = text.slice(8,11)
+    var monthNumber = monthObj[newText]
+    
+    
+    console.log(newText)
+    console.log(text)
+    console.log(monthNumber)
+    console.log(yearNumber)
+    console.log(dayNumber)
+    
+
+
+
+var submittedDate = yearNumber + '-' + monthNumber + '-' + dayNumber 
+// console.log(submittedDate)
+
+var submitDateUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=" + submittedDate.trim() + "&api_key=vbBdCkMZKqwb4qJwGH4og1wD1L5EJKlW5gF4m9m4"
+console.log(submitDateUrl)
+
+fetch(submitDateUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    var selectedPhoto = data.photos[0]
+
+    marsImgSrc.src = selectedPhoto.img_src
+
+    console.log(selectedPhoto.rover.name)
+    
+    solInfo.innerHTML = "Rover: " + selectedPhoto.rover.name
+    earthDate.innerText = "Earth Date: " + selectedPhoto.earth_date
+    camera.innerText = "Camera: " + selectedPhoto.camera.full_name
+    roverStatus.innerText = "Rover Status: " + selectedPhoto.rover.status
+
+
+  })
+
+
+}
+
+  
+  
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 $( function() {
   $( "#date-picker" ).datepicker({dateFormat:'yy-mm-dd'});
